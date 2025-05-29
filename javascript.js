cd31a3953872070a89593e0e002f902e4a49bc20
Nicholas Caudill <nick.c@coppermountaintech.com>
2025-05-29 15:47:37 -0400

    Add function 'playRound'

--- javascript.js
+++ javascript.js
@@ -8,5 +8,45 @@ function getHumanChoice() {
     return prompt("Please enter your choice between the following options: (rockrock, paper, scissors)");
 }
 
+function playRound(humanChoice, computerChoice) {
+    const human = humanChoice.toLowerCase();
+    const computer = computerChoice.toLowerCase();
+    
+    if (human === computer) {
+        console.log("You tied!");
+    }
+    else {
+        result = "";
+
+        if (human === "rock" && computer === "paper") {
+            result = "You lose! Paper beats Rock";
+            computerScore++;
+        }
+        else if (human === "rock" && computer === "scissors") {
+            result = "You win! Rock beats Scissors";
+            humanScore++;
+        }
+        else if (human === "paper" && computer === "rock") {
+            result = "You win! Paper beats Rock";
+            humanScore++;
+        }
+        else if (human === "paper" && computer === "scissors") {
+            result = "You lose! Scissors beats Paper";
+            humanScore++;
+        }
+        else if (human === "scissors" && computer === "rock") {
+            result = "You lose! Rock beats Rock";
+            humanScore++;
+        }
+        else if (human === "scissors" && computer === "paper") {
+            result = "You win! Scissors beats Paper";
+            humanScore++;
+        }
+        console.log(result);
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
+
+playRound(getHumanChoice(), getComputerChoice())
